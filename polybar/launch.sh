c7c7bad85c5af761f0a3225de43c95f91a220fd2
Zhe Li <zhe@minatjanster.se>
2021-04-10 09:25:41 +0200

    Add bottombar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,6 +11,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload example &
+        MONITOR=$m polybar --reload bottombar &
     done
 else
     polybar --reload example &
