f4dd062bd6ac9d032167649045fce36c7971b1e4
Zhe Li <zhe@minatjanster.se>
2021-04-26 08:58:23 +0200

    Move things around a bit

--- polybar/scripts/scroll_spotify_status.sh
+++ polybar/scripts/scroll_spotify_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see man zscroll for documentation of the following parameters
-zscroll -l 30 \
+zscroll -l 20 \
         --delay 0.1 \
         --scroll-padding "  " \
         --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
