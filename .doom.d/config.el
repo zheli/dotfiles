466e025481d8af1e4a0a13d162508fa712422baf
Zhe Li <zhe@gateway.fm>
2022-10-19 08:47:46 +0200

    Add org-agenda-file setting

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Zhe Li"
+      user-mail-address "linuxcity.jn@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -41,7 +41,9 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
+(setq org-agenda-files '("~/Dropbox/gtd/inbox.org"
+                         "~/Dropbox/gtd/gtd.org"
+                         "~/Dropbox/gtd/tickler.org"))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
