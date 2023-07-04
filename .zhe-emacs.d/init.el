ee9e88da03868cb5519c73665f79d02cc34d9c55
Zhe Li <zhe@gateway.fm>
2023-07-04 15:06:25 +0200

    chore(init.el): add Melpa package archive feat(init.el): install and enable Evil package chore(init.el): add custom-set-variables and custom-set-faces sections

--- .zhe-emacs.d/init.el
+++ .zhe-emacs.d/init.el
@@ -23,9 +23,31 @@
 
 (package-initialize)
 (require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 (setq package-enable-at-startup nil)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
