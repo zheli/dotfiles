12f74dfdf6de746947a3d05c0dcf73ebc2d167ab
Zhe Li <zhe@gateway.fm>
2023-07-20 21:20:28 +0200

    feat(.doom.d/packages.el): add smudge package for Spotify control
    The smudge package is added to provide Spotify control functionality. This package allows controlling Spotify playback directly from Emacs.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -8,6 +8,8 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 (package! hcl-mode)
+; Spotify control
+(package! smudge)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
